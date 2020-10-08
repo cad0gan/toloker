958324cf261e6f1e733d9e380cd8ff5c4a5954c6
Philip Minakov <sir.cadogan@protonmail.com>
2020-10-08 20:30:03 +0400

    Added argument parser

--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
 import asyncio
+import argparse
 from pytoloka import Toloka
 from config import Config
 from auto_accept import AutoAccept
 
 
 if __name__ == '__main__':
-    config = Config()
-    toloka = Toloka()
-    if asyncio.run(toloka.login(config.username, config.password)):
-        asyncio.run(AutoAccept(toloka)())
+    parser = argparse.ArgumentParser()
+    parser.add_argument('action', nargs='*')
+    args = parser.parse_args()
+
+    if args.action == 'accept':
+        config = Config()
+        toloka = Toloka()
+        if asyncio.run(toloka.login(config.username, config.password)):
+            asyncio.run(AutoAccept(toloka)())
