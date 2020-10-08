7c6f7f8d229035f46feea9c9924808e1a24a6b5f
Philip Minakov <sir.cadogan@protonmail.com>
2020-10-08 20:34:02 +0400

    Added -v flag to command line arguments

--- src/main.py
+++ src/main.py
@@ -4,14 +4,23 @@ from pytoloka import Toloka
 from config import Config
 from auto_accept import AutoAccept
 
+VERSION = '0.0.1'
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('action', nargs='*')
+    parser.add_argument('action', nargs='?', help='[accept]')
+    parser.add_argument('-v', '--version', action='store_true', help='show version and exit')
     args = parser.parse_args()
 
-    if args.action == 'accept':
-        config = Config()
-        toloka = Toloka()
-        if asyncio.run(toloka.login(config.username, config.password)):
-            asyncio.run(AutoAccept(toloka)())
+    if args.action:
+        if args.action == 'accept':
+            config = Config()
+            toloka = Toloka()
+            if asyncio.run(toloka.login(config.username, config.password)):
+                asyncio.run(AutoAccept(toloka)())
+        else:
+            print(f'Unknown action: {args.action}')
+            exit(1)
+    else:
+        if args.version:
+            print(VERSION)
