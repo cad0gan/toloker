b611a9d226a520f68fc81d882ebdc7bfaa9b0242
Philip Minakov <sir.cadogan@protonmail.com>
2020-10-08 16:08:01 +0400

    Auto accept toloka tasks

---
+++ src/auto_accept.py
@@ -0,0 +1,30 @@
+import sys
+from pytoloka import Toloka
+
+
+class AutoAccept:
+    def __init__(self, toloka: Toloka) -> None:
+        self._toloka = toloka
+
+    async def __call__(self, *args, **kwargs) -> None:
+        count = 0
+
+        while True:
+            toloka_tasks = await self._toloka.get_tasks()
+            count += 1
+            print(f'Requests: {count}')
+            sys.stdout.write('\033[F')
+            sys.stdout.write('\033[K')
+            for task in toloka_tasks:
+                if task['projectMetaInfo'].get('bookmarked'):
+                    if task['pools'][0].get('activeAssignments'):
+                        print('Active task: {}'.format(task['title']))
+                    else:
+                        pool_id = task['pools'][0]['id']
+                        print('Activating task: {}'.format(task['title']))
+
+                        result = await self._toloka.assign_task(pool_id, task['refUuid'])
+                        if not result.get('id'):
+                            res = await self._toloka.assign_task(pool_id, task['refUuid'])
+                        if result.get('id'):
+                            print('Activated task: {}'.format(task['title']))
--- src/main.py
+++ src/main.py
@@ -1,2 +1,11 @@
+import asyncio
+from pytoloka import Toloka
+from config import Config
+from auto_accept import AutoAccept
+
+
 if __name__ == '__main__':
-    pass
+    config = Config()
+    toloka = Toloka()
+    if asyncio.run(toloka.login(config.username, config.password)):
+        asyncio.run(AutoAccept(toloka)())
