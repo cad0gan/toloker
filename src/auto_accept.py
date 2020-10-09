90d049aa90015f5c9f1c10e0db42b69f7c6806ef
Philip Minakov <sir.cadogan@protonmail.com>
2020-10-09 12:32:42 +0400

    Catch exception

--- src/auto_accept.py
+++ src/auto_accept.py
@@ -1,5 +1,7 @@
 import sys
+import asyncio
 from pytoloka import Toloka
+from pytoloka.exceptions import HttpError
 from notify import Notify
 
 
@@ -16,27 +18,30 @@ class AutoAccept:
     async def __call__(self, *args, **kwargs) -> None:
         count = 0
         while True:
-            if self._exit:
-                break
+            try:
+                if self._exit:
+                    break
 
-            toloka_tasks = await self._toloka.get_tasks()
-            count += 1
-            print(f'Requests: {count}')
-            sys.stdout.write('\033[F')
-            sys.stdout.write('\033[K')
-            for task in toloka_tasks:
-                title = task['title']
+                toloka_tasks = await self._toloka.get_tasks()
+                count += 1
+                print(f'Requests: {count}')
+                sys.stdout.write('\033[F')
+                sys.stdout.write('\033[K')
+                for task in toloka_tasks:
+                    title = task['title']
 
-                if task['projectMetaInfo'].get('bookmarked'):
-                    if task['pools'][0].get('activeAssignments'):
-                        print('Active task: {}'.format(title))
-                    else:
-                        pool_id = task['pools'][0]['id']
-                        print('Activating task: {}'.format(title))
+                    if task['projectMetaInfo'].get('bookmarked'):
+                        if task['pools'][0].get('activeAssignments'):
+                            print('Active task: {}'.format(title))
+                        else:
+                            pool_id = task['pools'][0]['id']
+                            print('Activating task: {}'.format(title))
 
-                        result = await self._toloka.assign_task(pool_id, task['refUuid'])
-                        if not result.get('id'):
                             result = await self._toloka.assign_task(pool_id, task['refUuid'])
-                        if result.get('id'):
-                            print('Activated task: {}'.format(title))
-                            Notify()(subtitle='Activated task', message=title)
+                            if not result.get('id'):
+                                result = await self._toloka.assign_task(pool_id, task['refUuid'])
+                            if result.get('id'):
+                                print('Activated task: {}'.format(title))
+                                Notify()(subtitle='Activated task', message=title)
+            except HttpError:
+                await asyncio.sleep(1)
