374de07b300aee4d85518705dbd8d796e72a71f5
Philip Minakov <sir.cadogan@protonmail.com>
2020-10-13 15:50:43 +0400

    Command line subparsers

--- src/main.py
+++ src/main.py
@@ -11,32 +11,39 @@ VERSION = '0.2.0'
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    # parser.add_argument('action', nargs='?', help='[accept]')
-    parser.add_argument('-l', '--list-tasks', action='store_true', help='show all tasks')
-    parser.add_argument('-s', '--list-skills', action='store_true', help='show your skills')
+    subparsers = parser.add_subparsers(dest='subparser')
+
+    parser_tasks = subparsers.add_parser('tasks')
+    parser_tasks.add_argument('-l', '--list-tasks', action='store_true', help='show all tasks')
+
+    parser_skills = subparsers.add_parser('skills')
+    parser_skills.add_argument('-l', '--list-skills', action='store_true', help='show all skills')
+
     parser.add_argument('-v', '--version', action='store_true', help='show version and exit')
     args = parser.parse_args()
 
-    if args.list_tasks:
-        try:
-            toloka = Toloka()
-            if login(toloka):
-                tasks = asyncio.run(toloka.get_tasks())
-                for task in tasks:
-                    print(task['title'])
-        except HttpError:
-            exit(1)
-    elif args.list_skills:
-        try:
-            toloka = Toloka()
-            if login(toloka):
-                skills = asyncio.run(toloka.get_skills())
-                for skill in skills:
-                    print('{}: {}'.format(skill['skillName'], skill['value']))
-        except HttpError:
-            exit(1)
-    elif args.version:
+    if args.version:
         print(VERSION)
+    elif args.subparser == 'tasks':
+        if args.list_tasks:
+            try:
+                toloka = Toloka()
+                if login(toloka):
+                    tasks = asyncio.run(toloka.get_tasks())
+                    for task in tasks:
+                        print(task['title'])
+            except HttpError:
+                exit(1)
+    elif args.subparser == 'skills':
+        if args.list_skills:
+            try:
+                toloka = Toloka()
+                if login(toloka):
+                    skills = asyncio.run(toloka.get_skills())
+                    for skill in skills:
+                        print('{}: {}'.format(skill['skillName'], skill['value']))
+            except HttpError:
+                exit(1)
     else:
         try:
             toloka = Toloka()
