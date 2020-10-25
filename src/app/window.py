a400d1810c09085132b7650f306a57bf25d25ecd
Philip Minakov <sir.cadogan@protonmail.com>
2020-10-25 22:41:10 +0400

    Support of user input

--- src/app/window.py
+++ src/app/window.py
@@ -1,3 +1,4 @@
+import time
 import curses
 import signal
 import asyncio
@@ -13,16 +14,38 @@ class Window:
         self._stdout: StdOut = StdOut()
         self._thread: Thread = Thread(target=self._handle_keypress)
         self._telegram_bot: TelegramBot = TelegramBot()
+        self._input: bool = False
+
+    def input(self, text: str) -> str:
+        self._input = True
+        curses.nocbreak()
+        curses.echo()
+        curses.curs_set(True)
+        self._screen.nodelay(False)
+
+        y, x = curses.getsyx()
+        self._screen.addstr(y, x, text)
+        result: bytes = self._screen.getstr()
+
+        self._screen.nodelay(True)
+        curses.cbreak()
+        curses.noecho()
+        curses.curs_set(False)
+        curses.flushinp()
+        self._input = False
+        return result.decode('utf-8')
 
     def _handle_keypress(self) -> None:
         while True:
-            ch: int = self._screen.getch()
-            if ch == ord('q'):
-                self._worker.exit()
-                self._telegram_bot.exit()
-                break
-            elif ch == ord('s'):
-                self._worker.pause()
+            if not self._input:
+                ch: str = self._screen.getch()
+                if ch == ord('q'):
+                    self._worker.exit()
+                    self._telegram_bot.exit()
+                    break
+                elif ch == ord('s'):
+                    self._worker.pause()
+            time.sleep(0.5)
 
     def __call__(self) -> None:
         self._screen: any = curses.initscr()
@@ -35,12 +58,13 @@ class Window:
         except curses.error:
             pass
         self._screen.clear()
+        self._screen.nodelay(True)
         self._stdout.set()
         signal.signal(signal.SIGINT, lambda signum, frame: None)
         self._thread.start()
 
         async def run():
-            await asyncio.gather(self._worker(), self._telegram_bot())
+            await asyncio.gather(self._worker(self.input), self._telegram_bot())
         asyncio.run(run())
 
     def __del__(self) -> None:
