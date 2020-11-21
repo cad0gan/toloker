d3be9dcd94349239ae2d0bcc25d9dff34397fa3a
Philip Minakov <sir.cadogan@protonmail.com>
2020-11-21 06:10:48 +0400

    Fixed working with gnu screen over ssh

--- src/app/window.py
+++ src/app/window.py
@@ -15,6 +15,7 @@ class Window:
         self._worker: any = worker
         self._worker.input = self.input
         self._window: any = None
+        self._window_input: any = None
         self._stdout: StdOut = StdOut()
         self._thread: Thread = Thread(target=self._handle_keypress)
         self._input: bool = False
@@ -29,7 +30,7 @@ class Window:
 
     async def input(self, text: str) -> str:
         self._input = True
-        _input = Input(self._window)
+        _input = Input(self._window_input)
         try:
             result: str = await _input(text)
         finally:
@@ -37,7 +38,8 @@ class Window:
         return result
 
     def __call__(self) -> None:
-        self._window: any = curses.initscr()
+        self._window = curses.initscr()
+        self._window_input = curses.newwin(1, 1)
         curses.cbreak()
         curses.noecho()
         curses.curs_set(False)
@@ -47,7 +49,7 @@ class Window:
         except curses.error:
             pass
         self._window.clear()
-        self._window.nodelay(True)
+        self._window_input.nodelay(True)
         self._stdout.set()
         signal.signal(signal.SIGINT, lambda signum, frame: None)
         self._thread.start()
@@ -70,7 +72,7 @@ class WindowAssigner(Window):
     def _handle_keypress(self) -> None:
         while True:
             if not self._input:
-                ch: str = self._window.getch()
+                ch: str = self._window_input.getch()
                 if ch == ord('q'):
                     self._worker.exit()
                     self._telegram_bot.exit()
